31144a040fceaa0e922b29fc39d853b1ca637fb8
redwin <edwin.ravennes@ynov.com>
2025-09-25 16:51:14 +0200

    feat(feat-fonction-b): added 1 function

--- feat-fonction-b.go
+++ feat-fonction-b.go
@@ -4,3 +4,8 @@ package projetgitavanceroyerravennes
 func maman() string {
 	return "Edwin arrete de jouer a tes jeux de merde \n"
 }
+
+// league of legends is a trash game
+func taunt() string {
+	return "Bla-bla-bla-bla jg diff \n"
+}
