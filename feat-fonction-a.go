579003e9ad73357a90f78ab5265aca7f1a3a5e7a
rnicolas <nicolas.royer@ynov.com>
2025-09-25 16:30:31 +0200

    feat(feat-fonction-a): added a function salmon that talk about the tastes of our Délégay(eés)s and add some modification on the README

--- feat-fonction-a.go
+++ feat-fonction-a.go
@@ -11,3 +11,9 @@ func toilet() string {
 	return "tout a fini dans les toilettes de Léo \n"
 	return "les pauvres"
 }
+
+// the taste of our Délégay(eés)s
+func salmon() string {
+	return "le Délégay(eés)s aime le saumon a la mayo \n"
+	return "il aimerais bientot en manger"
+}
